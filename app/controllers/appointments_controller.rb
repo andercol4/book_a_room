37862e9f164a1b9161c648a83ee34afbb43d15ed
Colin Anderson <andercol.4@gmail.com>
2015-12-19 10:32:26 -0700

    basic controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,9 @@
 class AppointmentsController < ApplicationController
+  before_action: :find_appointment, except: [:index, :new, :create]
   def index
+    @room1 = Appointment.where(room_number: 1)
+    @room2 = Appointment.where(room_number: 2)
+    @room3 = Appointment.where(room_number: 3)
   end
 
   def show
@@ -8,6 +12,42 @@ class AppointmentsController < ApplicationController
   def new
   end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
+  end
+
   def edit
   end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to appointment_path(@appointment)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @appointment.destroy
+      redirect_to appointments_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:first_name, :last_name, :number_in_party, :email, :room_number, :start_time, :end_time, :email_conformation, :description)
+  end
+
+  def find_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
 end
